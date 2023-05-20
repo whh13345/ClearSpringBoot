c5b5612ecf45393259d0e6ad5f71b1e1247c8f77
hdf <383083270@qq.com>
2023-05-20 22:51:30 +0800

    first commit

---
+++ src/main/java/com/hdf/clearspringboot/ClearSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.hdf.clearspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClearSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClearSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hdf/clearspringboot/ClearSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hdf.clearspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClearSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
